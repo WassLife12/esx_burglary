cfb0927499f1ffb4a5c3ec9b4df58e58f891653d
WassLife12 <60321013+WassLife12@users.noreply.github.com>
2021-01-31 18:32:40 +0100

    Add files via upload

---
+++ esx_burglary/burglary.sql
@@ -0,0 +1,8 @@
+USE `essentialmode`;
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('ring', 'Bague', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('watch', 'Regarder', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('necklace', 'Collier en or', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('laptop', 'Ordinateur portable', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('camera', 'Camera', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('lockpick', 'Crochet', -1, 0, 1);
---
+++ esx_burglary/locales/en.lua
@@ -0,0 +1,8 @@
+Locales ['en'] = {
+	['used_kit']					= 'Vous avez utilisé 1 crochet.',
+	['must_be_outside']				= 'Vous ne pouvez pas faire cela à l intérieur du véhicule!',
+	['no_vehicle_nearby']			= 'Aucun véhicule à proximité',
+	['vehicle_unlocked']			= 'Vous avez réussi à déverrouiller le véhicule!',
+	['abort_hint']					= ' Pour annuler ~INPUT_VEH_DUCK~ appuyez sur la touche',
+	['aborted_lockpicking']		    = 'Vous avez annulé le processus.',
+}
